bcccd52743cfe7745288cf5965a333c33399d7aa
s19455 <s19455@pjwstk.edu.pl>
2025-03-13 18:33:28 +0100

    Implementation of method ObliczSrednia

--- APBD_App/Program.cs
+++ APBD_App/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello Jurassic World 3");
+
+public static double ObliczSrednia(int[] liczby)
+{
+    return liczby.Average();
+}
+
